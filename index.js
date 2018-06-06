089061f3ba55507ba8257aaf883c897353c1bf51
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-06-06 18:34:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function produceDrivingRange(blockRange) {
     if (distance <= blockRange) {
       return `within range by ${blockRange - distance}`
     } else {
-      return ''
+      return `${distance - blockRange} blocks out of range`
     }
   }
 }
