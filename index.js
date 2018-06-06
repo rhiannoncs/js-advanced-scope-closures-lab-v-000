844733dc7ec92099ffd708a79001c6309c6157c3
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-06-06 18:31:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(blockRange) {
   return function(block1, block2){
-    const distance = Math.abs(block1.slice(0, 1) - block2.slice(0, 1));
+    const distance = Math.abs(block1.slice(0, 2) - block2.slice(0, 2));
     if (distance <= blockRange) {
       return `within range by ${blockRange - distance}`
     }
