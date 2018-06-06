c58f88c9eb0d77890b88a355b76101e0236188df
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-06-06 18:33:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function produceDrivingRange(blockRange) {
     const distance = Math.abs(block1.slice(0, 2) - block2.slice(0, 2));
     if (distance <= blockRange) {
       return `within range by ${blockRange - distance}`
+    } else {
+      
     }
   }
 }
